89cd74f41a4f34d4f31051897e3c9df635833680
saiprakash0506 <konapasaip@gmail.com>
2026-01-13 16:01:08 +0530

    second commit

--- Python Selenium Notes/Day1_13Jan2026.py
+++ Python Selenium Notes/Day1_13Jan2026.py
@@ -362,7 +362,7 @@
 
 #map(function,iterable)--- syntax
 
-numbers=[1,2,3,4,5]
-result=map(lambda x:x*2,numbers)
-print(list(result))
+# numbers=[1,2,3,4,5]
+# result=map(lambda x:x*2,numbers)
+# print(list(result))
 
