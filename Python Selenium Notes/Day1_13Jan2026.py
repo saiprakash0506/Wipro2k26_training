441a73f9c67b9b21fa341ba0fc63a6a12aaccd09
saiprakash0506 <konapasaip@gmail.com>
2026-01-13 17:26:31 +0530

    c

--- Python Selenium Notes/Day1_13Jan2026.py
+++ Python Selenium Notes/Day1_13Jan2026.py
@@ -367,3 +367,39 @@
 # print(list(result))
 
 
+#exception handling
+
+#builtin exceptions
+
+# try:
+#     a=10
+#     b=0
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("Can't divide by Zero")
+
+
+# try:
+#     x=int(input("Enter a number"))
+#     print(10/x)
+# except ValueError:
+#     print("invalid entry")
+# except ZeroDivisionError:
+#     print("can't divide by zero")
+
+#user-defined expections
+class MyError(Exception):
+    pass
+#raise MyError("This is a user defined Exception")
+
+class invalidage(Exception):
+    pass
+
+try:
+    age=int(input("Enter your age"))
+    if age<18:
+        raise invalidage("Age must be 18 or above")
+    else:
+        print("eligible to vote")
+except invalidage as e:
+    print("Error :",e)
