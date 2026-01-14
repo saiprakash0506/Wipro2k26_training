cd7e70c242153b9400c105af01eb296cac377e98
saiprakash0506 <konapasaip@gmail.com>
2026-01-14 12:55:53 +0530

    third commit

---
+++ Assignment/Day2_Assignment.py
@@ -0,0 +1,82 @@
+#!Question – Iterators and Generators
+
+#& 1. Create a custom iterator class that iterates over numbers from 1 to N
+
+class Number:
+    def __init__(self,limit):
+        self.limit=limit
+        self.current=1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current<=self.limit:
+            val=self.current
+            self.current+=1
+            return val
+        else:
+            raise StopIteration
+        
+obj=Number(int(input("Enter any number:")))
+for i in obj:
+    print(i)
+
+#& 2. Create a generator function that yields the first N Fibonacci numbers.
+
+def fibonacci_num(n):
+    a, b = 0, 1
+    count = 0
+
+    while count < n:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+obj=fibonacci_num(5)
+for i in obj:
+    print(i,end=" ")
+
+#& 3. Demonstrate the difference between using the iterator and generator by printing values using a for loop
+
+#! iterator example
+
+class FibonacciIterator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.a = 0
+        self.b = 1
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < self.limit:
+            val = self.a
+            self.a, self.b = self.b, self.a + self.b
+            self.count += 1
+            return val
+        else:
+            raise StopIteration
+
+obj=FibonacciIterator(5)
+for i in obj:
+    print(i,end=" ")
+
+
+#generator example
+def fibonacci_generator(n):
+    a, b = 0, 1
+    count = 0
+
+    while count < n:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+for i in fibonacci_generator(10):
+    print(i,end=" ")
+
+#!  Question – Descriptors
+
---
+++ Python_Selenium_Notes/Day2_14Jan2026.py
@@ -0,0 +1,199 @@
+# #& Functions in Python 
+
+# # Defining a function
+
+# def add(a,b): 
+#     return a+b 
+
+# print(add(1,23))  
+
+# def add(d,e):
+#     print(d+e)
+
+# add(21.32,32)
+
+
+# def hello(name):
+#     return f"greetings {name}"
+
+# print(hello("sai prakash"))
+
+
+# #$args parameters - arguments
+
+# def print_args(*args):
+#     print(args)
+
+# print("hi")
+# print("saiprakash")
+# print(32)
+
+# #$*Kwargs - keyword args
+
+# def print_param1(**kwargs):
+#     print(kwargs)
+
+# print_param1(x=1,y=232,z="saiprakash")
+    
+
+# #! Module
+
+# import math as m
+
+# from math import sqrt,cos,sin
+
+# print(sqrt(9))
+# print(sin(180))
+# print(cos(0))
+
+
+#! OOPS object oriented programing
+
+#& creating class without the constructor
+
+# class Student: #class creation  
+#     name="saiprakash"
+#     age=22
+
+# s1=Student()  #object creation
+# print(f"my name is {s1.name} and my age is  {s1.age}")
+
+
+#& creating class with the help of constructor
+
+# class Employee:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+# e1=Employee("sai",23)
+# e2=Employee("prakash",24)
+# print(e1.name)
+# print(e1.age)
+# print(e1.name+e2.name)
+# print(e1.age+e2.age)
+# #print(e1+e2) #error
+
+
+#! Iterator
+
+# data=[1,2,3,4,5]
+# it=iter(data)
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+#& for loop has builtin iterator.
+
+# data=[1,2,3,4,5,6]
+# for x in data:
+#     print(x,end="")
+
+
+#& custom iterator
+
+# class Count:
+#     def __init__(self,limit):
+#         self.limit=limit
+#         self.current=1 
+
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         if self.current<=self.limit:
+#             val=self.current
+#             self.current+=1
+#             return val
+#         else:
+#             raise StopIteration
+# obj=Count(3)
+
+# for num in obj:
+#     print(num)
+
+        
+#! Generator -- using yield keyword
+
+# simple way of creating an iterator using yield keyword.
+
+# def numbers():
+#     yield 1
+#     yield 2
+#     yield 3
+
+# gen=numbers()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+
+#* example
+
+# def count_up(n):
+#     for i in range(1,n+1):
+#         yield i 
+
+# for val in count_up(10):
+#     print(val,end="")
+
+
+
+#! File handling
+
+#* this is reading and printing in terminal
+
+# file=open("Python Selenium Notes/file.txt","r")
+# content=file.readline()
+# content1=file.readlines()
+
+# print(content)
+# print(content1)
+
+# file.close()
+
+#* this is appending the txt into text file
+
+# file=open("Python Selenium Notes/file.txt","a")
+
+# file.write("\n New line added")
+
+# file.close()
+
+#* this is writing into the file-- this deletes the previos data and writes new
+
+
+# file=open("Python Selenium Notes/file.txt","w")
+
+# file.write("Hi this is write mode" )
+# file.write("Hi this is write mode second time" )
+
+# file.close()
+
+
+#! file handling in json file
+
+import json
+
+data={
+    "name":"saiprakashreddy",
+    "age":25,
+    "skills":["python","selenium"],
+    "place":"Hyderabad"
+}
+
+with open("Python_Selenium_Notes/data.json","w") as file:
+    json.dump(data,file,indent=18)
+
+
+#file handling in csv
+
+# import csv
+
+# with open("Python_Selenium_Notes/student.csv","w",newline="") as file:
+#     writer=csv.writer(file)
+#     writer.writerow(["Name","Id","Age"])
+#     writer.writerow(["saiprakash",1,22])
+#     writer.writerow(["prakash",2,82])
