96710787f49a22e81e0ae4f251cb3173d4772735
saiprakash0506 <konapasaip@gmail.com>
2026-01-22 12:34:27 +0530

    first commit

--- Assignment/DAY8/Day8_Assignment.py
+++ Assignment/DAY8/Day8_Assignment.py
@@ -16,3 +16,42 @@ Write a Python program that:
 5. Handles HTTP errors using proper exception handling.			
 '''
 
+import requests
+
+import json
+
+url = "https://jsonplaceholder.typicode.com/users"
+
+headers = {
+    "Accept": "application/json",
+    "User-Agent": "Python-Requests-App"
+}
+try:
+    response=requests.get(url,headers=headers)
+    
+    response.raise_for_status()
+
+    users=response.json()
+
+    extracted_data=[]
+    
+    for user in users:
+        extracted_data.append({
+            "id": user["id"],
+            "name": user["name"],
+            "email": user["email"]
+        })
+        
+    with open("Assignment/DAY8/users_data.json", "w") as file:
+        json.dump(extracted_data, file, indent=4)   
+        
+    print("data saved successfully")
+
+except requests.exceptions.HTTPError as e:
+    print("HTTP Error :",e) 
+
+except requests.exceptions.RequestException as e:
+    print("HTTP Error : ",e)
+    
+
+
