e740557dc0eb1052be1ba02604ef89695be184d7
saiprakash0506 <konapasaip@gmail.com>
2026-01-29 10:01:37 +0530

    commit

---
+++ Python_Selenium_Notes/DAY11/test_1.py
@@ -0,0 +1,67 @@
+import pytest
+
+#simple assert
+
+def test_addition():
+    print("addition done")
+    assert 2+3==5
+    assert 2+5==7
+
+#assert with message
+
+# def test_subtraction():
+#     print("subtraction done")
+#     assert 5-3==1,"subtraction result is incorrect"
+
+#exception handling in pytest
+
+
+def divide(a,b):
+    print("\n division done")
+    return a/b
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(10,0)
+
+
+#markers
+
+#! smoke mark
+@pytest.mark.smoke
+def test_login():
+    assert  True
+
+#! skip mark
+
+
+@pytest.mark.skip(reason="feature not implemented")
+def test_payment():
+    assert True
+
+#! conditional skip
+
+import sys
+
+@pytest.mark.skipif(sys.platform=="win32",reason="not supported in windows")
+def test_linux_only():
+    assert True
+
+
+#! expected failure
+
+
+@pytest.mark.xfail(reason="known bug")
+def test_know_issue():
+    assert 4+4==10
+
+
+#calculator.py
+
+def multiply(a,b):
+    return a*b
+
+def test_multiply():
+    assert multiply(3,4)==12
+
+    
