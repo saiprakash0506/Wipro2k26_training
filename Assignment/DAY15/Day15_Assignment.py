ecfb9e0474467f81096da6959e9053db95abc8a3
saiprakash0506 <konapasaip@gmail.com>
2026-02-03 16:11:51 +0530

    commit

---
+++ Assignment/DAY15/Day15_Assignment.py
@@ -0,0 +1,35 @@
+#! Question 1 – Selenium WebDriver Basics
+
+'''
+
+Write a Selenium WebDriver script that:
+
+1. Opens a Chrome or Firefox browser
+
+2. Navigates to https://example.com
+
+3. Prints the page title and URL
+
+4. Closes the browser
+
+'''
+
+#Ans ---> refer selenium project "Question1" - or in the folder Question1 you can find the pytest file and screenshot of it
+
+
+#! Question 2 – Selenium IDE Export
+
+'''Using Selenium IDE, create a test case that:
+
+1. Opens a browser and navigates to https://example.com/login
+
+2. Enters username and password
+
+3. Clicks the login button
+
+4. Validates the successful login message
+
+5. Export the test to Python WebDriver format and run it
+'''
+
+#Ans ---> refer selenium project "Question2" - or in the folder Question2 you can find the pytest file and screenshot of it
--- Python_Selenium_Notes/DAY15/Day15_3Feb2026.py
+++ Python_Selenium_Notes/DAY15/Day15_3Feb2026.py
@@ -1,1 +1,9 @@
 # Selenium Web Driver framework
+
+#! install the selenium ide extension in any browser
+
+#& steps:
+
+#   1)  open the extension and create new project.
+#   2) put the url there  "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login" 
+#   3) now try to start recording option and try to playback then you will understand how it works
