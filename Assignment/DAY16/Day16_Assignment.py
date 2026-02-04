35996a4d63e64268ef1ae00dee4e4eaf8215bd8f
saiprakash0506 <konapasaip@gmail.com>
2026-02-04 15:13:12 +0530

    commit

--- Assignment/DAY16/Day16_Assignment.py
+++ Assignment/DAY16/Day16_Assignment.py
@@ -8,5 +8,24 @@
 3. Validates a message displayed on the page		
 '''
 
-#Ans: refer "Question1" folder 
+#& Ans: refer "Question1 folder"  
 
+
+#! Question 4 – Browser Navigation
+
+'''
+Write a Selenium WebDriver script that:
+
+1. Opens a browser and navigates to https://example.com
+
+2. Navigates to another page on the same site
+
+3. Uses back(), forward(), and refresh()
+
+4. Prints the page title after each navigation
+
+5. Closes the browser
+
+'''
+
+#& Ans: refer  "Question2 folder"
