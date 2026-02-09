80be7cc21ce77e4f8e09ae7ae84d06fd7880105d
saiprakash0506 <konapasaip@gmail.com>
2026-02-09 16:43:41 +0530

    commit

---
+++ Assignment/DAY19/Day19_Assignment.py
@@ -0,0 +1,29 @@
+#! Question 9 – Selenium Waits	
+
+"""	
+Write a Selenium script that:	
+	
+1. Demonstrates implicit wait	
+	
+2. Demonstrates explicit wait for an element to become clickable	
+	
+3. Demonstrates fluent wait with a polling interval	
+	
+4. Prints a message when the element is available for interaction	"""
+
+#& Ans: refer Question1 folder
+
+#! Question 10 – Selenium Grid Execution
+"""
+Write a Selenium script that:
+
+1. Connects to Selenium Grid using RemoteWebDriver
+
+2. Runs the same test on multiple browsers
+
+3. Navigates to a website and verifies the page title
+
+4. Prints browser name and platform for each execution 
+"""
+
+#& Ans: refer Question2 folder
