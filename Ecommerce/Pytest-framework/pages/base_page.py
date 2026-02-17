c4fa90f41931094c227ef66e0b072a526d3a5cc6
saiprakash0506 <konapasaip@gmail.com>
2026-02-17 18:56:00 +0530

    commit

--- Ecommerce/Pytest-framework/pages/base_page.py
+++ Ecommerce/Pytest-framework/pages/base_page.py
@@ -10,7 +10,6 @@ class BasePage:
     def do_click(self, locator):
         element = self.wait.until(EC.element_to_be_clickable(locator))
         self.smooth_scroll(element)
-        time.sleep(0.5)
         element.click()
 
     def do_send_keys(self, locator, text):
@@ -18,24 +17,13 @@ class BasePage:
         self.smooth_scroll(element)
         element.clear()
         element.send_keys(text)
-        time.sleep(0.5)
-
-    def get_element_text(self, locator):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
-        self.smooth_scroll(element)
-        return element.text
 
     def smooth_scroll(self, element):
-        """Standard center-aligned smooth scroll."""
-        self.driver.execute_script(
-            "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", 
-            element
-        )
-        time.sleep(1.2) 
+        self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
+        time.sleep(1) 
 
     def slow_scroll_detailed(self):
-        """Specifically for the checkout summary to show price/tax clearly."""
-        for i in range(0, 550, 4):
+        for i in range(0, 500, 5):
             self.driver.execute_script(f"window.scrollTo(0, {i});")
-            time.sleep(0.03)
-        time.sleep(2.0)
+            time.sleep(0.02)
+        time.sleep(2)
