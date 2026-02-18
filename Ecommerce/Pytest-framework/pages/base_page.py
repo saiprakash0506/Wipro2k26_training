4fefb5dc952e19aef91ece2c752c1b941d5d6459
saiprakash0506 <konapasaip@gmail.com>
2026-02-18 18:04:52 +0530

    commit

--- Ecommerce/Pytest-framework/pages/base_page.py
+++ Ecommerce/Pytest-framework/pages/base_page.py
@@ -5,25 +5,26 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(self.driver, 15)
+        self.wait = WebDriverWait(self.driver, 10)
+        self.slow_delay = 0.6 
 
-    def do_click(self, locator):
-        element = self.wait.until(EC.element_to_be_clickable(locator))
-        self.smooth_scroll(element)
-        element.click()
+    def highlight(self, element, color="red"):
+        self.driver.execute_script(f"arguments[0].style.border='3px solid {color}'", element)
 
-    def do_send_keys(self, locator, text):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
-        self.smooth_scroll(element)
-        element.clear()
-        element.send_keys(text)
+    def click(self, locator):
+        el = self.wait.until(EC.element_to_be_clickable(locator))
+        self.highlight(el, "red")
+        time.sleep(self.slow_delay)
+        el.click()
 
+    def type(self, locator, text):
+        el = self.wait.until(EC.visibility_of_element_located(locator))
+        self.highlight(el, "blue")
+        el.clear()
+        time.sleep(self.slow_delay)
+        el.send_keys(text)
+
+    # ADDED THIS METHOD TO FIX YOUR ERROR
     def smooth_scroll(self, element):
         self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
-        time.sleep(1) 
-
-    def slow_scroll_detailed(self):
-        for i in range(0, 500, 5):
-            self.driver.execute_script(f"window.scrollTo(0, {i});")
-            time.sleep(0.02)
-        time.sleep(2)
+        time.sleep(0.5)
